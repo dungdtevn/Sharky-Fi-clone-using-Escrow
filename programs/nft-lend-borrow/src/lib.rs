cf2e9d999f0745569fed34c9e61b23c30690cb1e
dungdtevn <154307146+dungdtevn@users.noreply.github.com>
2023-12-19 14:21:02 +0700

    declare program id

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("6ZosN5UZT7smxcq2ZCVEtsq8BCpLvvsSLJrrv2PpszaE");
 
 #[program]
 pub mod nft_lend_borrow {
